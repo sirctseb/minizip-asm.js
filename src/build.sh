5f59f5f0d895d6d2155d15c8227f1dcdc62a84ea
Christopher Best <bestchris@gmail.com>
2017-10-10 12:47:12 -0400

    comment out zlib and aes build entirely

--- src/build.sh
+++ src/build.sh
@@ -16,28 +16,28 @@ mkdir lib
 echo "Make zlib..."
 cd zlib
 emconfigure ./configure
-emmake make
+# emmake make
 
 echo "Make aes..."
 cd contrib/minizip/aes
-emcc aescrypt.c -o aescrypt.o -O -DHAVE_AES
-emcc aeskey.c -o aeskey.o -O -DHAVE_AES
-emcc aestab.c -o aestab.o -O -DHAVE_AES
-emcc entropy.c -o entropy.o -O -DHAVE_AES
-emcc fileenc.c -o fileenc.o -O -DHAVE_AES
-emcc hmac.c -o hmac.o -O -DHAVE_AES
-emcc prng.c -o prng.o -O -DHAVE_AES
-emcc pwd2key.c -o pwd2key.o -O -DHAVE_AES
-emcc sha1.c -o sha1.o -O -DHAVE_AES
-emcc aescrypt.o aeskey.o aestab.o entropy.o fileenc.o hmac.o prng.o pwd2key.o sha1.o -o ../libaes.o
+# emcc aescrypt.c -o aescrypt.o -O
+# emcc aeskey.c -o aeskey.o -O
+# emcc aestab.c -o aestab.o -O
+# emcc entropy.c -o entropy.o -O
+# emcc fileenc.c -o fileenc.o -O
+# emcc hmac.c -o hmac.o -O
+# emcc prng.c -o prng.o -O
+# emcc pwd2key.c -o pwd2key.o
+# emcc sha1.c -o sha1.o -O
+# emcc aescrypt.o aeskey.o aestab.o entropy.o fileenc.o hmac.o prng.o pwd2key.o sha1.o -o ../libaes.o
 
 echo "Make minizip..."
 cd ..
-emcc zip.c -o zip.o -O -I../.. -DHAVE_AES
-emcc unzip.c -o unzip.o -O -I../.. -DHAVE_AES
-emcc ioapi_mem.c -o ioapi_mem.o -O -I../.. -DHAVE_AES
-emcc ioapi.c -o ioapi.o -O -I../.. -DHAVE_AES
-emcc -s USE_ZLIB=1 -O -I../.. -DHAVE_AES -o ../../../lib/minizip.js main.cpp --memory-init-file 0 -s ALLOW_MEMORY_GROWTH=1 -s NO_FILESYSTEM=0 -s EXPORTED_FUNCTIONS="['_list', '_extract', '_append']" --pre-js main_pre.js --post-js main_post.js zip.o unzip.o ioapi.o ioapi_mem.o libaes.o -Oz
+emcc zip.c -o zip.o -O -I../..
+emcc unzip.c -o unzip.o -O -I../..
+emcc ioapi_mem.c -o ioapi_mem.o -O -I../..
+emcc ioapi.c -o ioapi.o -O -I../..
+emcc -s USE_ZLIB=1 -O -I../.. -o ../../../lib/minizip.js main.cpp --memory-init-file 0 -s ALLOW_MEMORY_GROWTH=1 -s NO_FILESYSTEM=0 -s EXPORTED_FUNCTIONS="['_list', '_extract', '_append']" --pre-js main_pre.js --post-js main_post.js zip.o unzip.o ioapi.o ioapi_mem.o -Oz
 
 echo "Finished Build!\nPlease type command 'npm run finalbuild' to complete build."
 cd ../../..
